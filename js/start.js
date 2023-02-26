47475c5438e873e4a28274c0c32b44885f06af07
Joinsider <johannespopp2006@gmail.com>
2023-02-26 20:44:24 +0100

    Changed Start JS and start.html

--- js/start.js
+++ js/start.js
@@ -1,4 +1,26 @@
+/*
+Vorläufiger Code für später
+var playercountinput = document.getElementById('playerscount');
+
+playercountinput.addEventListener()
+
+*/
+
 function checkplayercount() {
-    var playercount = document.getElementById(players-count).innerHTML;
-    document.getElementById(changetoplayercount).innerHTML(playercount);
+    var playercount = document.getElementById(playerscount);
+    if(playercount = 1){
+        document.getElementById(colorplayer1).style.display = "block";
+    } else if (playercount = 2) {
+        document.getElementById(colorplayer1).style.display = "block";
+        document.getElementById(colorplayer2).style.display = "block";
+    } else if (playercount = 3) {
+        document.getElementById(colorplayer1).style.display = "block";
+        document.getElementById(colorplayer2).style.display = "block";
+        document.getElementById(colorplayer3).style.display = "block";
+    }else {
+        document.getElementById(colorplayer1).style.display = "block";
+        document.getElementById(colorplayer2).style.display = "block";
+        document.getElementById(colorplayer3).style.display = "block";
+        document.getElementById(colorplayer4).style.display = "block";
+    }
 }
